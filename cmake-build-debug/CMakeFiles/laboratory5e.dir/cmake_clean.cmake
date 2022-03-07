ef003176a2de9db627a45ee34a3142236622bb77
serayaGirya <sergey_girko@mail.ru>
2022-03-07 19:36:36 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/laboratory5e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laboratory5e.dir/main.c.obj"
+  "laboratory5e.exe"
+  "laboratory5e.exe.manifest"
+  "laboratory5e.pdb"
+  "liblaboratory5e.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laboratory5e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/laboratory5e.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for laboratory5e.
+# This may be replaced when dependencies are built.
